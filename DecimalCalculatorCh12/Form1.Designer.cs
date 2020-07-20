22ef0a045287a6edbb58100ebd9d03785aa2fc15
Local User <Local User@DESKTOP-N1NMMQ0>
2020-07-20 01:53:27 -0400

    Add project files.

---
+++ DecimalCalculatorCh12/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace DecimalCalculatorCh12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(131, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(57, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(376, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(131, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(252, 103);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(57, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(376, 103);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(57, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(131, 193);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(57, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(252, 193);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(57, 50);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(376, 193);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(57, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(252, 289);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(57, 50);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // divideButton
+            // 
+            this.divideButton.Location = new System.Drawing.Point(498, 322);
+            this.divideButton.Name = "divideButton";
+            this.divideButton.Size = new System.Drawing.Size(72, 39);
+            this.divideButton.TabIndex = 10;
+            this.divideButton.Text = "/";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.divideButton_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(445, 367);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(125, 35);
+            this.calculateButton.TabIndex = 11;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(203, 389);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(43, 13);
+            this.resultLabel.TabIndex = 12;
+            this.resultLabel.Text = "Result: ";
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(252, 386);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(83, 20);
+            this.resultTextBox.TabIndex = 10;
+            this.resultTextBox.TextChanged += new System.EventHandler(this.resultTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 482);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.divideButton);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ DecimalCalculatorCh12/Form1.cs
@@ -0,0 +1,190 @@
+﻿//Programmer: Austin Parker
+//Date: June 22,2020
+//Assignment: Ch12/ PE 8/ Decimal Calculator
+//Purpose: created a calculator for PE 7 to show example of exception handling for division in PE8
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecimalCalculatorCh12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //declared variables
+        decimal numerator;
+        decimal denominator;
+        string num = "";
+        string denom = "";
+        bool flag = false;
+        decimal result;
+
+        //Connects number buttons 0-9 using if statements
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "1";
+            }
+            else
+            {
+                denom = "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "2";
+            }
+            else
+            {
+                denom = "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "3";
+            }
+            else
+            {
+                denom = "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "4";
+            }
+            else
+            {
+                denom = "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "5";
+            }
+            else
+            {
+                denom = "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "6";
+            }
+            else
+            {
+                denom = "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "7";
+            }
+            else
+            {
+                denom = "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "8";
+            }
+            else
+            {
+                denom = "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "9";
+            }
+            else
+            {
+                denom = "9";
+            }
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (flag == false)
+            {
+                num += "0";
+            }
+            else
+            {
+                denom = "0";
+            }
+
+        }
+        
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            flag = true;
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Converts numbers to decimal
+            numerator = Convert.ToDecimal(num);
+            denominator = Convert.ToDecimal(denom);
+
+            //Here is where program will throw the error and notify user "0" action is not possible in calculator
+            try
+            {
+                result = numerator / denominator;
+                resultTextBox.Text = Convert.ToString(result);
+            }
+
+            catch
+            { 
+                MessageBox.Show("Division by zero is not possible because it's a non-integral value");
+            }
+        }
+
+        private void resultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Text = string.Empty;
+        }
+    }
+}
+
